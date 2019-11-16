7b3c9ea060fc523c1b18ba17a74e809af520f6f3
Amel Musić <amel.music@gmail.com>
2019-11-16 10:18:56 +0100

    V1_2_3

---
+++ StudentskaSluzba/StudentskaSluzba/Baza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentskaSluzba
+{
+    static class Baza
+    {
+        static public List<Korisnik> Korisnici { get; set; } = new List<Korisnik>();
+
+        static public bool Login(string username, string password)
+        {
+            foreach(var item in Korisnici)
+            {
+                if (item.Username.Equals(username, StringComparison.InvariantCultureIgnoreCase)
+                    && item.Password == password)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
